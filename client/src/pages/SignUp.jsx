73092622a4372371f51d77fb98f8182506d959ba
Sobirov3636 <sobirov3636@gmail.com>
2023-12-08 22:37:19 +0900

    complete sign in page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -43,7 +43,7 @@ export default function SignUp() {
   };
   return (
     <div className='max-w-lg p-3 mx-auto'>
-      <h1 className='text-3xl font-semibold text-center my-7'>SignUp</h1>
+      <h1 className='text-3xl font-semibold text-center my-7'>Sign Up</h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input
           type='text'
