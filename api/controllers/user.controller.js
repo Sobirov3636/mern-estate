a3edb0d2e5060ed8f8dae126cafd1c9e784c1dc3
Sobirov3636 <sobirov3636@gmail.com>
2023-12-27 22:13:00 +0900

    create get user listings api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import Listing from "../models/listing.model.js";
 
 export const test = (req, res) => {
   res.json({ message: "Api is working" });
@@ -39,12 +40,27 @@ export const updateUser = async (req, res, next) => {
 // Delete user function
 
 export const deleteUser = async (req, res, next) => {
-  if (req.user.id !== req.params.id) return next(errorHandler(401, "You can only update your own account"));
+  if (req.user.id === req.params.id) return next(errorHandler(401, "You can only update your own account"));
   try {
     await User.findByIdAndDelete(req.params.id);
     res.clearCookie("access_token");
     res.status(200).json("User has been deleted!");
   } catch (error) {
     next(error);
+  }
+};
+
+// Get user listings function
+
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id !== req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, "You can only view your own listings!"));
   }
 };
